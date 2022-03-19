b558a3af01d3e48ee30c0328ec79fdd99812b058
Ken Kyle <88218089+klhkyle@users.noreply.github.com>
2022-03-18 23:37:21 -0600

    Add files via upload

---
+++ air_hockey/game/casting/surface.py
@@ -0,0 +1,34 @@
+from constants import *
+from game.casting.actor import Actor
+
+
+class Surface(Actor):
+    """A solid, spherical object that is bounced around in the game."""
+    
+    def __init__(self, body, image, debug = False):
+        """Constructs a new Puck.
+
+        Args:
+            body: A new instance of Body.
+            image: A new instance of Image.
+            debug: If it is being debugged. 
+        """
+        super().__init__(debug)
+        self._body = body
+        self._image = image
+
+    def get_body(self):
+        """Gets the puck's body.
+        
+        Returns:
+            An instance of Body.
+        """
+        return self._body
+
+    def get_image(self):
+        """Gets the puck's image.
+        
+        Returns:
+            An instance of Image.
+        """
+        return self._image
