f8bdc056e6700ed2358b2ae8cdb13a9d231fcdf8
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-03-30 22:59:04 -0300

    Update collide_borders_action.py

--- air_hockey/game/scripting/collide_borders_action.py
+++ air_hockey/game/scripting/collide_borders_action.py
@@ -1,7 +1,9 @@
 from constants import *
 from game.casting.sound import Sound
 from game.scripting.action import Action
-
+"""
+This module handles the events in which the puck hits the borders
+"""
 
 class CollideBordersAction(Action):
 
@@ -17,30 +19,32 @@ class CollideBordersAction(Action):
         y = position.get_y()
         slide_hit = Sound(SLIDE_HIT)
         goal = Sound(GOAL)
+
         #TODO: Check if puck is in the border
-        if puck == SCREEN_HEIGHT:
-            self._audio_service.play_sound(slide_hit)
-            body.set_position(x, FIELD_TOP)
-            body.set_velocity(0, 0)
 
-        if puck == SCREEN_WIDTH:
-            self._audio_service.play_sound(slide_hit)
-            body.set_position(FIELD_RIGHT, y)
-            body.set_velocity(0, 0)
+        # if puck == SCREEN_HEIGHT:
+        #     self._audio_service.play_sound(slide_hit)
+        #     body.set_position(x, FIELD_TOP)
+        #     body.set_velocity(0, 0)
+
+        # if puck == SCREEN_WIDTH:
+        #     self._audio_service.play_sound(slide_hit)
+        #     body.set_position(FIELD_RIGHT, y)
+        #     body.set_velocity(0, 0)
 
 
-        if x < FIELD_LEFT:
+        if x < (FIELD_LEFT + 45):
             puck.bounce_x()
             self._audio_service.play_sound(slide_hit)
 
-        if x >= (FIELD_RIGHT - PUCK_WIDTH):
+        if x >= (FIELD_RIGHT - PUCK_WIDTH - 45):
             puck.bounce_x()
             self._audio_service.play_sound(slide_hit)
 
-        if y < FIELD_TOP:
+        if y < (FIELD_TOP + 30):
             puck.bounce_y()
             self._audio_service.play_sound(slide_hit)
 
-        if y >= (FIELD_BOTTOM - PUCK_WIDTH):
+        if y >= (FIELD_BOTTOM - PUCK_WIDTH - 30):
             callback.on_next(GAME_OVER)
             self._audio_service.play_sound(goal)
