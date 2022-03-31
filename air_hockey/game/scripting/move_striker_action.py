61e48f21493b022b64fc4c2f9a6b50df3a650371
Ken Kyle <88218089+klhkyle@users.noreply.github.com>
2022-03-31 17:44:44 -0600

    Update move_striker_action.py

--- air_hockey/game/scripting/move_striker_action.py
+++ air_hockey/game/scripting/move_striker_action.py
@@ -17,13 +17,19 @@ class MoveStrikerAction(Action):
         velocity = body.get_velocity()
         position = body.get_position()
         x = position.get_x()
+        y = position.get_y()
         
         position = position.add(velocity)
+        
 #This will allow us to make it to bounce
         if x < 0:
             position = Point(0, position.get_y())
         elif x > (SCREEN_WIDTH - STRIKER_WIDTH):
             position = Point(SCREEN_WIDTH - STRIKER_WIDTH, position.get_y())
+        elif y < 0:
+            position = Point(position.get_x(), 0)
+        elif y > (SCREEN_HEIGHT - STRIKER_HEIGHT):
+            position = Point(position.get_x(), SCREEN_HEIGHT - STRIKER_HEIGHT)
             
         body.set_position(position)
 
@@ -39,13 +45,17 @@ class MoveStrikerAction2(Action):
         velocity2 = body2.get_velocity()
         position2 = body2.get_position()
         x2 = position2.get_x()
+        y2 = position2.get_y()
         
         position2 = position2.add(velocity2)
 
         if x2 < 0:
-            position2= Point(0, position2.get_y())
+            position2 = Point(0, position2.get_y())
         elif x2 > (SCREEN_WIDTH - STRIKER_WIDTH):
             position2 = Point(SCREEN_WIDTH - STRIKER_WIDTH, position2.get_y())
+        elif y2 < 0:
+            position2 = Point(position2.get_x(), 0)
+        elif y2 > (SCREEN_HEIGHT - STRIKER_HEIGHT):
+            position2 = Point(position2.get_x(), SCREEN_HEIGHT - STRIKER_HEIGHT)
             
         body2.set_position(position2)
-        
