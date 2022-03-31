cb8ce2437f3e5c96fa6f08ec71b354b5b040792d
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-03-31 11:48:47 -0300

    Update __main__.py

--- air_hockey/__main__.py
+++ air_hockey/__main__.py
@@ -2,7 +2,10 @@ from constants import *
 from game.directing.director import Director
 from game.directing.scene_manager import SceneManager
 
-
+"""
+MAIN: The main file is used to execute the game. Just hit "run" and enjoy the magic 
+Disclaimer: Sometimes some errors may come out, we are terrible sorry about that, we did our best :)
+"""
 def main():
     director = Director(SceneManager.VIDEO_SERVICE)
     director.start_game()
